f31630625a1e9bfeae9f80def98e3f04a63e5d54
Joseph Budney <jo.s.budney@baypath.net>
2023-11-11 15:18:39 -0500

    works with keyboard. added commented code to work with button. using sleep instead of my own function. added the images

--- merlins.py
+++ merlins.py
@@ -1,12 +1,26 @@
 import cv2
 import time
+import keyboard
+#import RPi.GPIO as GPIO
 
-merlin_1 = cv2.imread('images/merlin-mouse')
-merlin_2 = cv2.imread('images/tv-merlin')
-merlin_3 = cv2.imread('images/merlin-shrek')
-black = cv2.imread('images/black')
+#GPIO.setmode(GPIO.BCM)
 
-merlist = [merlin_1, merlin_2, merlin_3]
+#button_GPIO = 23
+
+#GPIO.setup(button_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+merlist = []
+
+merlist.append(cv2.imread('images/Picture1.jpg'))
+merlist.append(cv2.imread('images/Picture2.jpg'))
+merlist.append(cv2.imread('images/Picture3.png'))
+merlist.append(cv2.imread('images/Picture4.png'))
+merlist.append(cv2.imread('images/Picture5.jpg'))
+merlist.append(cv2.imread('images/Picture6.jpg'))
+merlist.append(cv2.imread('images/Picture7.jpg'))
+merlist.append(cv2.imread('images/Picture8.jpg'))
+merlist.append(cv2.imread('images/Picture9.jpg')) 
+
 
 wait_time = 0
 
@@ -15,14 +29,22 @@ cv2.setWindowProperty('merlins',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
 while True:
 
-    cv2.imshow('merlins', black)
+    cv2.imshow('merlins', cv2.imread('images/black'))
+
 
     cv2.waitKey(0)
 
+    if keyboard.is_pressed('esc'):
+        cv2.destroyAllWindows()
+        break
+    
+    #if GPIO.input(button_GPIO) == False:
     for merlin in merlist:
         start = time.time()
         cv2.imshow('merlins', merlin)
         cv2.waitKey(1)
-        while wait_time <= 2:
-            wait_time = time.time()-start
-        wait_time = 0
+        time.sleep(1)
+        # while wait_time <= 1:
+        #     wait_time = time.time()-start
+        # wait_time = 0
+        
--- pattern.py
+++ pattern.py
@@ -1,28 +1,64 @@
 import cv2
 import time
 import keyboard
+#import RPi.GPIO as GPIO
 
+#GPIO.setmode(GPIO.BCM)
 
-merlin_1 = cv2.imread('images/merlin-mouse')
-merlin_2 = cv2.imread('images/tv-merlin')
-merlin_3 = cv2.imread('images/merlin-shrek')
-black = cv2.imread('images/black')
+#button_GPIO = 23
 
-merlist = [merlin_1, merlin_2, merlin_3]
+# def wait(seconds): 
+#     wait_time = 0
+#     start = time.time()
+#     while wait_time <= seconds:
+#         wait_time = time.time()-start
 
-wait_time = 0
+def button_with_image(image, input):
+    if keyboard.is_pressed(input): # change to rpi button
+    #if GPIO.input(input) == False:
+        cv2.imshow('merlins', image)
+        cv2.waitKey(1)
+        #wait(1) # this function call can change how long the pictures are shown for
+        time.sleep(1)
+        current_sequence.append(input)
+        if current_sequence[-1] != correct_sequence[len(current_sequence)-1]:
+            return False
+
+merlist = []
+
+merlist.append(cv2.imread('images/Picture1.jpg'))
+merlist.append(cv2.imread('images/Picture2.jpg'))
+merlist.append(cv2.imread('images/Picture3.png'))
+merlist.append(cv2.imread('images/Picture4.png'))
+merlist.append(cv2.imread('images/Picture5.jpg'))
+merlist.append(cv2.imread('images/Picture6.jpg'))
+merlist.append(cv2.imread('images/Picture7.jpg'))
+merlist.append(cv2.imread('images/Picture8.jpg'))
+merlist.append(cv2.imread('images/Picture9.jpg')) 
+
+correct_sequence = ['1','2','3','4','5','6','7','8','9']
+
+current_sequence = []
+
+buttons = ['1','2','3','4','5','6','7','8','9']
 
 cv2.namedWindow('merlins', cv2.WINDOW_NORMAL)
 cv2.setWindowProperty('merlins',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
 while True:
 
-    cv2.imshow('merlins', black)
+    cv2.imshow('merlins', cv2.imread('images/black'))
     cv2.waitKey(1)
 
-    if keyboard.is_pressed('q'):
-        cv2.imshow('merlins', merlin_1)
-        cv2.waitKey(1)
-    if keyboard.is_pressed('esc'):
-        cv2.waitKey(1)
+    for x in range(len(buttons)):
+        if button_with_image(merlist[x], buttons[x]) == False:
+            current_sequence = []
+
+    if current_sequence == correct_sequence:
+        print("win")
         cv2.destroyAllWindows
+        break
+
+    if keyboard.is_pressed('esc'):
+        cv2.destroyAllWindows
+        break
