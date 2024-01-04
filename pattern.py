6c69634c56a5c297096d08f95dd26c97fa3ebc4e
Joseph Budney <jo.s.budney@baypath.net>
2024-01-04 15:11:00 -0500

    updated pattern.py

--- pattern.py
+++ pattern.py
@@ -1,24 +1,17 @@
 import cv2
 import time
 import keyboard
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-#GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-#button_GPIO = 23
-
-# def wait(seconds): 
-#     wait_time = 0
-#     start = time.time()
-#     while wait_time <= seconds:
-#         wait_time = time.time()-start
+button_GPIO = 23
 
 def button_with_image(image, input):
-    if keyboard.is_pressed(input): # change to rpi button
     #if GPIO.input(input) == False:
+    if keyboard.is_pressed(input):
         cv2.imshow('merlins', image)
         cv2.waitKey(1)
-        #wait(1) # this function call can change how long the pictures are shown for
         time.sleep(1)
         current_sequence.append(input)
         if current_sequence[-1] != correct_sequence[len(current_sequence)-1]:
@@ -26,15 +19,15 @@ def button_with_image(image, input):
 
 merlist = []
 
-merlist.append(cv2.imread('images/Picture1.jpg'))
-merlist.append(cv2.imread('images/Picture2.jpg'))
-merlist.append(cv2.imread('images/Picture3.png'))
-merlist.append(cv2.imread('images/Picture4.png'))
-merlist.append(cv2.imread('images/Picture5.jpg'))
-merlist.append(cv2.imread('images/Picture6.jpg'))
-merlist.append(cv2.imread('images/Picture7.jpg'))
-merlist.append(cv2.imread('images/Picture8.jpg'))
-merlist.append(cv2.imread('images/Picture9.jpg')) 
+merlist.append(cv2.resize(cv2.imread('images/Picture1.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture2.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture3.png'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture4.png'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture5.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture6.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture7.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture8.jpg'), (1080,1080)))
+merlist.append(cv2.resize(cv2.imread('images/Picture9.jpg'), (1080,1080))) 
 
 correct_sequence = ['1','2','3','4','5','6','7','8','9']
 
@@ -47,7 +40,7 @@ cv2.setWindowProperty('merlins',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
 while True:
 
-    cv2.imshow('merlins', cv2.imread('images/black'))
+    cv2.imshow('merlins', cv2.resize(cv2.imread('images/black'), (1080,1080)))
     cv2.waitKey(1)
 
     for x in range(len(buttons)):
